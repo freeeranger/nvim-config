58d3b887645a856366c37e504524bb6144907d43
Freeranger <freeranger05@gmail.com>
2024-02-20 09:25:13 +0100

    fixed telescope live grep

--- lua/freeranger/after/telescope.lua
+++ lua/freeranger/after/telescope.lua
@@ -67,7 +67,5 @@ local builtin = require("telescope.builtin")
 
 vim.keymap.set("n", "<leader>.", builtin.find_files, {})
 vim.keymap.set("n", "<C-p>", builtin.git_files, {})
-vim.keymap.set("n", "<leader>fs", function()
-	builtin.grep_string({ search = vim.fn.input("Grep: ") })
-end)
+vim.keymap.set("n", "<leader>fs", builtin.live_grep, {})
 vim.keymap.set("n", "<leader>,", ":Telescope file_browser path=%:p:h select_buffer=true<CR>", { noremap = true })
