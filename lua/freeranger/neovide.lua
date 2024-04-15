769c8e92aa323ae0985eb8f5c691831cc536edb3
freeeranger <freeranger05@gmail.com>
2024-04-15 21:05:24 +0200

    neovide improvements

--- lua/freeranger/neovide.lua
+++ lua/freeranger/neovide.lua
@@ -1,8 +1,11 @@
 if vim.g.neovide then
-    vim.o.guifont = "JetBrainsMono Nerd Font:h11"
+	vim.o.guifont = "JetBrainsMono Nerd Font:h9.75"
+	vim.g.neovide_refresh_rate = 144
+	vim.g_neovide_scale_factor = 0.8
+	vim.g.neovide_fullscreen = true
 
-    vim.g.neovide_scroll_animation_length = 0
-    vim.g.neovide_cursor_animation_length = 0
-    vim.g.neovide_remember_window_size = true
-    vim.g.neovide_cursor_vfx_mode = ""
+	vim.g.neovide_scroll_animation_length = 0
+	vim.g.neovide_cursor_animation_length = 0
+	vim.g.neovide_remember_window_size = true
+	vim.g.neovide_cursor_vfx_mode = ""
 end
