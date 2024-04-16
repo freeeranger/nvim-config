3013783fca544c21261d580028f021cc91c90b30
freeeranger <freeranger05@gmail.com>
2024-04-16 23:26:31 +0200

    disabled copilot, transparent bg

--- lua/freeranger/after/catppuccin.lua
+++ lua/freeranger/after/catppuccin.lua
@@ -1,6 +1,7 @@
 require("catppuccin").setup({
-    flavour = "mocha",
-    no_italic = true
+	flavour = "mocha",
+	no_italic = true,
+	transparent_background = true,
 })
 
-vim.cmd.colorscheme "catppuccin"
+vim.cmd.colorscheme("catppuccin")
